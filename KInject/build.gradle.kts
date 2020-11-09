19b9736a33da5bd03162eca7898674e117ef5aab
mledoxvii <marianoxvii@gmail.com>
2020-11-09 08:09:18 +0100

    chore: Create project

---
+++ KInject/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.mledoxvii"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
